12e7acf0bd6c259cccfade98ef3facdd67bcec08
SirThane <jerade.hensley@gmail.com>
2018-03-24 02:06:21 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import tile_data
+import csv
+
+
+# Warp tile = 0 or an empty CSV cell
+
+class AWMap:
+
+    def __init__(self, data, source):
+        self.data = data
+        self.size_w = 0
+        self.size_h = 0
+        self.style = 0
+        self.awmap = []
+        self.source = source
+        if self.source == "AWS":
+            self.bin_data = bytearray(self.data)
+            self.from_aws()
+
+    def from_aws(self):
+        # Width, Height, and graphic style
+        self.size_w, self.size_h, self.style = self.bin_data[10:13]
+        self.awmap = [[AWTile(x + 1, y + 1, self.terr_from_bin(x, y), self.unit_from_bin(x, y), self)
+                       for y in range(self.size_h)] for x in range(self.size_w)]
+
+    def terr_from_bin(self, x, y):
+        # Return 2 byte terrain value from binary data for coordinate (x, y)
+        offset = (y + (x * self.size_h)) * 2
+        return self.bin_data[offset:offset + 2]
+
+    def unit_from_bin(self, x, y):
+        # Return 2 byte unit value from binary data for coordinate (x, y)
+        offset = ((y + (x * self.size_h)) * 2) + (self.size_h * self.size_w * 2)
+        return self.bin_data[offset:offset + 2]
+
+    def tile(self, x, y):
+        # Return tile object at coordinate (x, y)
+        return self.awmap[x][y]
+
+
+class AWTile:  # TODO: Account for multi-tile terrain objects e.g. death ray, volcano, etc.
+
+    def __init__(self, x, y, terrain, unit, awmap):
+        self.x, self.y, self.unit, self.awmap = x, y, unit, awmap
+        if self.awmap.source == "AWS":
+            self.terr_bin = terrain
+
+    def tile(self, x, y):
+        return self.awmap.tile(x, y)
